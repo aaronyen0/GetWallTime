b904ae76a0e1e1d8574e0c1c8567f6d70808fe5a
worcdlo <37761249+worcdlo@users.noreply.github.com>
2018-08-21 11:53:43 +0800

    Create GetWallTime.c

---
+++ GetWallTime.c
@@ -0,0 +1,12 @@
+double get_wall_time(){
+    LARGE_INTEGER time,freq;
+    if (!QueryPerformanceFrequency(&freq)){
+        //  Handle error
+        return 0;
+    }
+    if (!QueryPerformanceCounter(&time)){
+        //  Handle error
+        return 0;
+    }
+    return (double)time.QuadPart / freq.QuadPart;
+}
