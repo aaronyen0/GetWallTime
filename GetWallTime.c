67e38d5aba8c632b8fb8739b066087bd2b117f2a
worcdlo <37761249+worcdlo@users.noreply.github.com>
2018-08-21 12:01:05 +0800

    Update GetWallTime.c

--- GetWallTime.c
+++ GetWallTime.c
@@ -1,5 +1,31 @@
+#include <Windows.h>
+
+static __int64 gTime = 0;
+
+void GetWallTimeStart(){
+    LARGE_INTEGER time;
+    if (!QueryPerformanceCounter(&time)){
+        return;
+    }
+    gTime = time.QuadPart;
+}
+
+double GetWallTimeEnd(){
+    LARGE_INTEGER time, freq;
+    if (!QueryPerformanceCounter(&time)){
+        return -1;
+    }
+	if (!QueryPerformanceFrequency(&freq)){
+        return -2;
+    }
+    gTime = time.QuadPart - gTime;
+	return (double)gTime / freq.QuadPart;
+}
+
+
+
 double get_wall_time(){
-    LARGE_INTEGER time,freq;
+    LARGE_INTEGER time, freq;
     if (!QueryPerformanceFrequency(&freq)){
         //  Handle error
         return 0;
